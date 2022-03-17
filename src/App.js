2d21b99e021ae3d41e12d3072f22c7fbbedb3eb6
Osama Rais <osamaraees98@hotmail.com>
2022-03-17 20:47:52 +0500

    Few Changes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,59 @@
-import React from 'react';
-import NewExpense from './components/NewExpense';
-import Expenses from './components/Expenses';
+import React, { useState } from "react";
+import NewExpense from "./components/NewExpense";
+import Expenses from "./components/Expenses";
 
 
+const Dummy_Expenses = [
+  {
+    id:'e1',
+    title: "Petrol",
+    amount: "$267",
+    date: new Date(2021, 11, 5),
+  },
+  {
+    id:'e2',
+    title: "Bills",
+    amount: "$384",
+    date: new Date(2021, 11, 5),
+  },
+  {
+    id:'e3',
+    title: "Rent",
+    amount: "$1000",
+    date: new Date(2021, 11, 5),
+  },
+  {
+    id:'e4',
+    title: "Car Insurance",
+    amount: "$200",
+    date: new Date(2021, 11, 5),
+  },
+  {
+    id:'e5',
+    title: "Medicines",
+    amount: "$54",
+    date: new Date(2021, 11, 5),
+  },
+  {
+    id:'e6',
+    title: "Shopping",
+    amount: "$85",
+    date: new Date(2021, 11, 5),
+  },
+];
 
 const App = () => {
+  const [expense, setExpenses] = useState(Dummy_Expenses);
 
-  const expense = [
-    {
-      title: "Petrol",
-      amount: "$267",
-      date: new Date(2021, 11, 5),
-    },
-    {
-      title: "Bills",
-      amount: "$384",
-      date: new Date(2021, 11, 5),
-    },
-    {
-      title: "Rent",
-      amount: "$1000",
-      date: new Date(2021, 11, 5),
-    },
-    {
-      title: "Car Insurance",
-      amount: "$200",
-      date: new Date(2021, 11, 5),
-    },
-    {
-      title: "Medicines",
-      amount: "$54",
-      date: new Date(2021, 11, 5),
-    },
-    {
-      title: "Shopping",
-      amount: "$85",
-      date: new Date(2021, 11, 5),
-    },
-  ];
-
-  const addExpenseHandler = expense =>  {
-    console.log('In App.js');
-    console.log(expense);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
-    <NewExpense onAddExpense={addExpenseHandler}/>
-    <Expenses items={expense} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expense} />
     </div>
   );
 };
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,49 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "./Card";
 import ExpenseItems from "./ExpenseItems";
 import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
-  const FilterChangeHandler = selectedYear => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const FilterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter={FilterChangeHandler} />
-        <ExpenseItems
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItems>
-        <ExpenseItems
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItems>
-        <ExpenseItems
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItems>
-        <ExpenseItems
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItems>
-        <ExpenseItems
-          title={props.items[4].title}
-          amount={props.items[4].amount}
-          date={props.items[4].date}
-        ></ExpenseItems>
-        <ExpenseItems
-          title={props.items[5].title}
-          amount={props.items[5].amount}
-          date={props.items[5].date}
-        ></ExpenseItems>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={FilterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItems
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
